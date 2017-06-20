95abc33870d48de9f9111d007371f0cc94c8b118
Joseph Puchalski <joeypuchalski@gmail.com>
2017-06-20 17:15:36 -0400

    Add seed for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!({username: "admin", password: 123456, name: "admin", email:"admin@admin.com", bio: "Administrator of the App" })
