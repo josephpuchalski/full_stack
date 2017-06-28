362a0a8470617e7c93bb30004b475db44ea6c094
Joseph Puchalski <joeypuchalski@gmail.com>
2017-06-28 17:52:50 -0400

    Style with css

--- frontend/components/feed/feed_item.jsx
+++ frontend/components/feed/feed_item.jsx
@@ -15,9 +15,20 @@ class FeedItem extends React.Component {
         return (
           <div className="feed-post-item">
             <div className='feed-post-header'>
-            <Link to={`/${this.props.post.user}`}><img src={this.props.post.profile_image} /><span>{this.props.post.user}</span></Link>
+              <Link to={`/${this.props.post.user}`}><img src={this.props.post.profile_image} /><span>{this.props.post.user}</span></Link>
             </div>
             <img key={this.props.post.id} src={this.props.post.image} />
+            <div className='feed-post-detail'>
+            <div>
+              <i className="fa fa-heart-o fa-2x" aria-hidden="true"></i>
+              <label htmlFor="comment"><i className="fa fa-commenting-o fa-2x" aria-label="true"></i></label>
+              </div>
+              <span>0 Likes</span>
+              <span>{new Date(this.props.post.created_at).toDateString()}</span>
+              <form>
+              <textarea id="comment" placeholder="Add Comment"></textarea>
+              </form>
+            </div>
           </div>
         );
       }
