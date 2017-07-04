31406b45418a8a6e09474b8fb6e2703721927ebc
Joseph Puchalski <joeypuchalski@gmail.com>
2017-07-04 16:57:18 -0400

    Prep for production

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,11 +3,20 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  // must use 'require' (import only allowed at top of file)
+  const createLogger = require('redux-logger');
+  middlewares.push(createLogger());
+}
+
 const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+    // applyMiddleware(thunk, logger)
+    applyMiddleware(...middlewares)
   )
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,67 @@
-const path = require('path');
+// const path = require('path');
+//
+// module.exports = {
+//   context: __dirname,
+//   entry: './frontend/travelscape.jsx',
+//   output: {
+//     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+//     filename: 'bundle.js'
+//   },
+//   resolve: {
+//     extensions: ['.js', '.jsx', '*']
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         test: /\.jsx?$/,
+//         exclude: /(node_modules)/,
+//         loader: 'babel-loader',
+//         query: {
+//           presets: ['react', 'es2015']
+//         }
+//       }
+//     ]
+//   },
+//   devtool: 'source-maps'
+// };
 
+var path = require("path");
+var webpack = require("webpack");
+
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
+
+// include plugins config
 module.exports = {
   context: __dirname,
-  entry: './frontend/travelscape.jsx',
+  entry: "./frontend/travelscape.jsx",
   output: {
-    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
+    filename: "bundle.js"
   },
-  resolve: {
-    extensions: ['.js', '.jsx', '*']
-  },
+  plugins: plugins,
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules)/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015']
@@ -22,5 +69,8 @@ module.exports = {
       }
     ]
   },
-  devtool: 'source-maps'
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
 };
